be584f56fafc7d3cfb41fd5384830a1511a67953
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-27 20:07:00 -0600

    stopping point with question

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // let body = document.getElementById("body")
 // let main = document.getElementById("main")
 let pushedArray = []
+let pokemonNames = []
 let fetchedData = []
 let fetchAPI = async (url) => {
 	const res = await fetch(url)
@@ -8,13 +9,17 @@ let fetchAPI = async (url) => {
 	pushArray(data)
 	return data
 }
-
+//making this fucntion instead of pushing where it is called after the await/async function allowed it to work. no idea why
 let pushArray = (data) => {
 	pushedArray.push(data)
-	console.log(pushedArray[0])
 }
 
+let pushPokemonNames = () => {
+	for (pokemon of pushedArray.length)
+		pokemonNames.push(pushedArray[0].results[0].name)
+}
 fetchAPI("https://pokeapi.co/api/v2/pokemon")
-
+//this is a good stopping point, why does the below line have the jumbled data instead of anmes?
+console.log("pokemonNames", pokemonNames)
 // console.log(fetchedData)
 // console.log("pushedArray", pushedArray.results)
