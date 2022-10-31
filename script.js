1c51d64c3686d0742259dd0b7c41b52e43bee45b
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-31 13:11:25 -0600

    numbersGenerator func working, converting to string

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // let body = document.getElementById("body")
-// check out lodash
-//
 let main = document.getElementById("main")
 let pokemonNames = []
 let fetchedData = []
 let numbers = []
-let accordianItem = `<div class="accordion-item">
-<h2 class="accordion-header" id="accordian">
+let numbersAsString = []
+
+let accordianItem = `<div class="accordion-item" id=${pokemonNames[0]}>
+<h2 class="accordion-header" id="headingOne">
   <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-	Accordion Item #1
+	PokemonName1
   </button>
 </h2>
-<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+<div id="collapse${pokemonNames[0]}" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
   <div class="accordion-body">
-	<strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+	<strong>Pokemon 1 is a badass
   </div>
 </div>
 </div>`
@@ -26,58 +26,44 @@ let fetchAPI = async (url) => {
 	}
 	const res = await fetch(url)
 	const data = await res.json()
-	//may not need pushArray anymore because of magic.
 	await pushPokemonNames(data)
 	let nextURL = data.next
 	await fetchAPI(nextURL)
-	await capitalizePokemonNames()
+	await numberArrayGenerator()
 }
 
 //will add numbers starting with one to numberWords array based on how many pokemon have been fetched
-let numberArrayGenerator = () => {
+let numberArrayGenerator = async () => {
 	for (let i = 0; i < pokemonNames.length; i++) {
-		numbers += numbers + i
+		numbers[i] = i
 	}
+	console.log(numbers)
 }
 //will generate HTML and modify html main to include a new accordian each time a pokemon is fetched
 let accordianGenerator = () => {
 	main = main + accordianItem
 }
-//q4e i converted pushPokemonNames to a for loop instead of a for of loop (both functions below)
-//----in what manner am I misusing the for of? results undefined when given [pokemon]
-//TODO good practice to convert this function to eitehr forEach, or map, or for of loop etc
-let capitalizePokemonNames = async () => {
-	for (let i = 0; i < pokemonNames.length; i++) {
-		let indyPokemonName = pokemonNames[i]
-		let firstLetter = indyPokemonName[0].toUpperCase()
-		let restOfLetters = indyPokemonName.slice(1)
-		let cappedName = firstLetter + restOfLetters
-		pokemonNames[i] = cappedName
-	}
-	console.log("capitalizePokemonNames()", pokemonNames)
-}
 
 let pushPokemonNames = async (data) => {
 	for (let singlePokemon of data.results) {
-		pokemonNames.push(singlePokemon.name)
+		pokemonNames.push(_.capitalize(singlePokemon.name))
 	}
 	console.log("pushPokemonNames()", pokemonNames)
 }
-// let pushPokemonNames = async () => {
-// 	for (i = 0; i < pushedArray[0].results.length; i++) {
-// 		for (pokemon of )
-// 		pokemonNames.push(pushedArray[].results[i].name)
-// 	}
-
-// 	console.log("pushPokemonNames()", pokemonNames)
-// }
-
-// let pushPokemonNames = () => {
-// 	for (pokemon of pushedArray[0].results) {
-// 		pokemonNames.push(pushedArray[0].results[pokemon].name)
-// 	}
-// 	console.log(pokemonNames)
-// }
 fetchAPI("https://pokeapi.co/api/v2/pokemon")
 
 //single pokemon from data 'pushedArray[0].results[0].name'
+
+//q4e i converted pushPokemonNames to a for loop instead of a for of loop (both functions below)
+//----in what manner am I misusing the for of? results undefined when given [pokemon]
+//TODO good practice to convert this function to eitehr forEach, or map, or for of loop etc
+// let capitalizePokemonNames = async () => {
+// 	for (let i = 0; i < pokemonNames.length; i++) {
+// 		let indyPokemonName = pokemonNames[i]
+// 		let firstLetter = indyPokemonName[0].toUpperCase()
+// 		let restOfLetters = indyPokemonName.slice(1)
+// 		let cappedName = firstLetter + restOfLetters
+// 		pokemonNames[i] = cappedName
+// 	}
+// 	console.log("capitalizePokemonNames()", pokemonNames)
+// }
