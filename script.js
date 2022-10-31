fae51f0972bef0e95d14c35cf60439d215c85a36
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-31 16:23:41 -0600

    searchBar works, converting to leave all html in place but open the pokemons accordian

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 // let body = document.getElementById("body")
 let main = document.getElementById("main")
 let pokemonAccordion = document.getElementById("pokemonAccordion")
+let searchBarText = document.getElementById("searchBarText")
+let searcehButton = document.getElementById("searchButton")
 let pokemonNames = []
 let fetchedData = []
 let numbers = []
 let numbersAsString = []
-
-//todo avoid recursive loop bugs by removing pushPokemonNames() until all data is fetched
-//only fetch if less than 151 pokemon
+let searchBarInnerText = searchBarText
+//initiates fetching of data and generation of html w pokemon
 let fetchAPI = async (url) => {
 	if (pokemonNames.length > 150) {
 		return null
 	}
-
 	const res = await fetch(url)
 	const data = await res.json()
 	await pushPokemonNames(data)
+	console.log(pokemonNames)
 	let nextURL = data.next
 	await fetchAPI(nextURL)
 }
@@ -37,27 +38,48 @@ let accordianItemGenerator = (pokemonName) => {
 	pokemonAccordion.innerHTML = pokemonAccordion.innerHTML + accordianItem
 }
 
+//pushes the name property into an array pokemonNames
 let pushPokemonNames = async (data) => {
 	for (let singlePokemon of data.results) {
 		let cappedName = _.capitalize(singlePokemon.name)
 		pokemonNames.push(cappedName)
-		accordianItemGenerator(cappedName)
+		if (cappedName === "Chikorita") {
+			return null
+		} else {
+			accordianItemGenerator(cappedName)
+		}
 	}
 }
-fetchAPI("https://pokeapi.co/api/v2/pokemon")
 
-//single pokemon from data 'pushedArray[0].results[0].name'
-
-//q4e i converted pushPokemonNames to a for loop instead of a for of loop (both functions below)
-//----in what manner am I misusing the for of? results undefined when given [pokemon]
-//TODO good practice to convert this function to eitehr forEach, or map, or for of loop etc
-// let capitalizePokemonNames = async () => {
-// 	for (let i = 0; i < pokemonNames.length; i++) {
-// 		let indyPokemonName = pokemonNames[i]
-// 		let firstLetter = indyPokemonName[0].toUpperCase()
-// 		let restOfLetters = indyPokemonName.slice(1)
-// 		let cappedName = firstLetter + restOfLetters
-// 		pokemonNames[i] = cappedName
-// 	}
-// 	console.log("capitalizePokemonNames()", pokemonNames)
+// let postSearchAccordianItemGenerator = (pokemonName) => {
+// 	let postSearchAccordianItem = `<div class="accordion-item" id="${pokemonName}">
+// 	<h2 class="accordion-header" id="heading${pokemonName}">
+// 	  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${pokemonName}" aria-expanded="true" aria-controls="collapse${pokemonName}">
+// 	  ${pokemonName}
+// 	  </button>
+// 	</h2>
+// 	<div id="collapse${pokemonName}" class="accordion-collapse collapse" aria-labelledby="heading${pokemonName}" data-bs-parent="#pokemonAccordion">
+// 	  <div class="accordion-body">
+// 		<strong>${pokemonName} is a badass</strong>
+// 	  </div>
+// 	</div>
+// 	</div>`
+// 	pokemonAccordion.innerHTML = postSearchAccordianItem
 // }
+
+searcehButton.addEventListener("click", function () {
+	let postSearchAccordianItem = `<div class="accordion-item" id="${searchBarText.value}">
+<h2 class="accordion-header" id="heading${searchBarText.value}">
+  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${searchBarText.value}" aria-expanded="true" aria-controls="collapse${searchBarText.value}">
+  ${searchBarText.value}
+  </button>
+</h2>
+<div id="collapse${searchBarText.value}" class="accordion-collapse collapse" aria-labelledby="heading${searchBarText.value}" data-bs-parent="#pokemonAccordion">
+  <div class="accordion-body">
+	<strong>${searchBarText.value} is a badass</strong>
+  </div>
+</div>
+</div>`
+	pokemonAccordion.innerHTML = postSearchAccordianItem
+})
+fetchAPI("https://pokeapi.co/api/v2/pokemon")
