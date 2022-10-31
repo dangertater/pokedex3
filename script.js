a6bd79b80c33ab41ab76df08ef8cdb6f60ac7402
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-31 14:06:00 -0600

    accordian working, next step limit accordian to 151 and add properties of each pokemon

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 // let body = document.getElementById("body")
 let main = document.getElementById("main")
+let pokemonAccordion = document.getElementById("pokemonAccordion")
 let pokemonNames = []
 let fetchedData = []
 let numbers = []
 let numbersAsString = []
-
-let accordianItem = `<div class="accordion-item" id=${pokemonNames[0]}>
-<h2 class="accordion-header" id="headingOne">
-  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-	PokemonName1
-  </button>
-</h2>
-<div id="collapse${pokemonNames[0]}" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-  <div class="accordion-body">
-	<strong>Pokemon 1 is a badass
-  </div>
-</div>
-</div>`
 
 //todo avoid recursive loop bugs by removing pushPokemonNames() until all data is fetched
 //only fetch if less than 151 pokemon
@@ -24,31 +12,37 @@ let fetchAPI = async (url) => {
 	if (pokemonNames.length > 150) {
 		return null
 	}
+
 	const res = await fetch(url)
 	const data = await res.json()
 	await pushPokemonNames(data)
 	let nextURL = data.next
 	await fetchAPI(nextURL)
-	await numberArrayGenerator()
 }
 
-//will add numbers starting with one to numberWords array based on how many pokemon have been fetched
-let numberArrayGenerator = async () => {
-	for (let i = 0; i < pokemonNames.length; i++) {
-		numbers[i] = i
-	}
-	console.log(numbers)
-}
 //will generate HTML and modify html main to include a new accordian each time a pokemon is fetched
-let accordianGenerator = () => {
-	main = main + accordianItem
+let accordianItemGenerator = (pokemonName) => {
+	let accordianItem = `<div class="accordion-item" id="${pokemonName}">
+	<h2 class="accordion-header" id="heading${pokemonName}">
+	  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${pokemonName}" aria-expanded="true" aria-controls="collapse${pokemonName}">
+	  ${pokemonName}
+	  </button>
+	</h2>
+	<div id="collapse${pokemonName}" class="accordion-collapse collapse" aria-labelledby="heading${pokemonName}" data-bs-parent="#pokemonAccordion">
+	  <div class="accordion-body">
+		<strong>${pokemonName} is a badass</strong>
+	  </div>
+	</div>
+	</div>`
+	pokemonAccordion.innerHTML = pokemonAccordion.innerHTML + accordianItem
 }
 
 let pushPokemonNames = async (data) => {
 	for (let singlePokemon of data.results) {
-		pokemonNames.push(_.capitalize(singlePokemon.name))
+		let cappedName = _.capitalize(singlePokemon.name)
+		pokemonNames.push(cappedName)
+		accordianItemGenerator(cappedName)
 	}
-	console.log("pushPokemonNames()", pokemonNames)
 }
 fetchAPI("https://pokeapi.co/api/v2/pokemon")
 
